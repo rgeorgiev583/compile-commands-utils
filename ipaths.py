3ba6982ade2d1b9b193d5531a6dc430f5d3ed543
Radoslav Georgiev <rgeorgiev583@gmail.com>
2020-08-02 23:28:17 +0300

    add implementation

---
+++ ipaths.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+import os.path
+import json
+import argparse
+import sys
+
+def get_relpath_with_prefix(path: str, prefix: str, curdir: str) -> str:
+    if os.path.isabs(path):
+        return os.path.relpath(path, start=curdir)
+
+    return os.path.normpath(os.path.join(prefix, path))
+
+def get_include_paths_from_compile_commands(path: str):
+    compile_commands_path = os.path.join(path, 'compile_commands.json')
+    try:
+        with open(compile_commands_path, 'r') as compile_commands_file:
+            compile_db_json = compile_commands_file.read()
+    except OSError:
+        print(format('error: file {} does not exist', compile_commands_path))
+        return
+
+    compile_commands = json.loads(compile_db_json)
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('-I', action='append', default=[], dest='include_paths')
+    for cmd_obj in compile_commands:
+        if os.path.isabs(cmd_obj['directory']):
+            dir_rel = os.path.relpath(cmd_obj['directory'], start=path)
+        else:
+            dir_rel = cmd_obj['directory']
+        file_dir = os.path.dirname(cmd_obj['file'])
+        print(get_relpath_with_prefix(file_dir, dir_rel, path))
+        parsed_args = argparser.parse_known_args(cmd_obj['command'].split())
+        for include_path in parsed_args[0].include_paths:
+            print(get_relpath_with_prefix(include_path, dir_rel, path))
+
+if len(sys.argv) == 1:
+    get_include_paths_from_compile_commands(os.path.curdir)
+else:
+    for path in sys.argv[1:]:
+        get_include_paths_from_compile_commands(path)

798a33c38752b2fa60ce6405cd816135751d5579
Radoslav Georgiev <rgeorgiev583@gmail.com>
2021-03-22 00:16:33 +0200

    use json.load() instead of json.loads(); format code

--- ipaths.py
+++ ipaths.py
@@ -5,24 +5,26 @@ import json
 import argparse
 import sys
 
+
 def get_relpath_with_prefix(path: str, prefix: str, curdir: str) -> str:
     if os.path.isabs(path):
         return os.path.relpath(path, start=curdir)
 
     return os.path.normpath(os.path.join(prefix, path))
 
+
 def get_include_paths_from_compile_commands(path: str):
     compile_commands_path = os.path.join(path, 'compile_commands.json')
     try:
         with open(compile_commands_path, 'r') as compile_commands_file:
-            compile_db_json = compile_commands_file.read()
+            compile_commands = json.load(compile_commands_file)
     except OSError:
         print(format('error: file {} does not exist', compile_commands_path))
         return
 
-    compile_commands = json.loads(compile_db_json)
     argparser = argparse.ArgumentParser()
-    argparser.add_argument('-I', action='append', default=[], dest='include_paths')
+    argparser.add_argument('-I', action='append',
+                           default=[], dest='include_paths')
     for cmd_obj in compile_commands:
         if os.path.isabs(cmd_obj['directory']):
             dir_rel = os.path.relpath(cmd_obj['directory'], start=path)
@@ -33,6 +35,7 @@ def get_include_paths_from_compile_commands(path: str):
         parsed_args = argparser.parse_known_args(cmd_obj['command'].split())
         for include_path in parsed_args[0].include_paths:
             print(get_relpath_with_prefix(include_path, dir_rel, path))
+
 
 if len(sys.argv) == 1:
     get_include_paths_from_compile_commands(os.path.curdir)

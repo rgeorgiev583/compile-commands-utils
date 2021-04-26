508244d4aedd8f99a7368cf98012c6a5823e2f6d
Radoslav Georgiev <rgeorgiev583@gmail.com>
2021-04-26 03:19:40 +0300

    fix a bug where the keys in the compiledb dict would not be updated after path substitution

--- merge_compile_commands.py
+++ merge_compile_commands.py
@@ -6,8 +6,7 @@ import os.path
 import sys
 
 from generate_header_compile_commands import (add_opts_to_argparser,
-                                              generate_header_compile_commands,
-                                              read_compile_commands_into_dict)
+                                              generate_header_compile_commands)
 
 
 def unescape_backslash_sequences(s: str) -> str:
@@ -25,22 +24,26 @@ argparser.add_argument(
     'compile_commands_file', nargs='+', default=os.path.curdir, help='path to `compile_commands.json` file to merge')
 args = argparser.parse_args()
 
-compile_commands_dict = {}
+compile_commands = []
 for compile_commands_file in args.compile_commands_file:
-    read_compile_commands_into_dict(
-        compile_commands_file, compile_commands_dict)
+    with open(compile_commands_file, 'r') as compile_commands_file:
+        compile_commands += json.load(compile_commands_file)
 
 if args.subst_path:
     for subst_path in args.subst_path:
         (src_path, dst_path) = subst_path.split(':')
         src_path = unescape_backslash_sequences(src_path)
         dst_path = unescape_backslash_sequences(dst_path)
-        for entry in compile_commands_dict.values():
+        for entry in compile_commands:
             for key in entry:
                 entry[key] = entry[key].replace(src_path, dst_path)
 
 if args.base_src_file:
+    compile_commands_dict = {}
+    for entry in compile_commands:
+        compile_commands_dict[entry['file']] = entry
     for entry in generate_header_compile_commands(args, compile_commands_dict):
         compile_commands_dict[entry['file']] = entry
+    compile_commands = list(compile_commands_dict.values())
 
-json.dump(list(compile_commands_dict.values()), sys.stdout)
+json.dump(compile_commands, sys.stdout)
